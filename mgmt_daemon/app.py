7fa955671cfb694be722bfab90e2a5044cdc8bd3
Chens <1836178267@qq.com>
2023-03-12 20:50:55 +0800

    upload

--- mgmt_daemon/app.py
+++ mgmt_daemon/app.py
@@ -1,3 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
+
+@app.route('/')
+def main_redirect():
+    return redirect('/login')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='20500')
